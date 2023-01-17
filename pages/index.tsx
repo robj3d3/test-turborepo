42d167cc67b0699e573b19e3441d8ccb0d56477d
Rob Hallam <roboffice100@gmail.com>
2023-01-17 16:00:09 +0000

    feat(index): Added a comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  // Just a normal comment
   return (
     <>
       <Head>
